74c6bdd93258411162c5a9bd4a47e526812ea8e2
root <yuziluo123@gmail.com>
2019-07-16 23:44:03 +0000

    linked to mongoDB Atlas

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ var commentRoutes     = require("./routes/comments"),
 	campgroundRoutes  = require("./routes/campgrounds"),
 	indexRoutes       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/angie_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://angela:1996yuziyuzi520@angiecamp-src3c.mongodb.net/test?retryWrites=true&w=majority", { 
+	useNewUrlParser: true, 
+	useCreateIndex: true
+}).then(() => {
+	console.log("Connected to DB");
+}).catch(err => {
+	console.log("Error: ", err.message);
+}); 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
